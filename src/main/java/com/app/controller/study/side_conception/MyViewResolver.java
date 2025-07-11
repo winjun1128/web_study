8ec4446c5274d4c5f8769db3e280d8809586eb57
winjun1128 <hsj1128ty@naver.com>
2025-07-11 17:01:26 +0900

    스프링 MVC 디스패쳐서블릿 작동 개념

---
+++ src/main/java/com/app/controller/study/side_conception/MyViewResolver.java
@@ -0,0 +1,18 @@
+package com.app.controller.study.side_conception;
+
+public class MyViewResolver {
+	String prefix;
+	String suffix;
+	
+	public MyViewResolver() {
+		prefix="/WEB-INF/views/";
+		suffix = ".jsp";
+	}
+	public String viewResove(String viewName) {
+		
+		//	viewName : mainpage
+		// "/WEB-INF/views/" + "mainpage" + ".jsp";
+		// /WEB-INF/views/mainpage.jsp
+		return prefix + viewName + suffix;
+	}
+}

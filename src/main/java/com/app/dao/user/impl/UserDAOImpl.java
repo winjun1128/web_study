a4f84223d1da2da989ee949883c64f34b78053b9
winjun1128 <hsj1128ty@naver.com>
2025-07-23 15:25:42 +0900

    사용자 로그인/로그아웃 처리 ,마이페이지 추가

--- src/main/java/com/app/dao/user/impl/UserDAOImpl.java
+++ src/main/java/com/app/dao/user/impl/UserDAOImpl.java
@@ -30,6 +30,13 @@ public class UserDAOImpl implements UserDAO {
 		
 		return userList;
 	}
+
+	@Override
+	public User findUserById(String id) {
+		
+		User user = sqlSessionTemplate.selectOne("user_mapper.findUserById",id);
+		return user;
+	}
 	
 	
 }

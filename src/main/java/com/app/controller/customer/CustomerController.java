717d3a1e78e115042095ffe694fdec80df7be474
winjun1128 <hsj1128ty@naver.com>
2025-07-29 12:46:51 +0900

    로그 설정 및 사용

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -19,7 +19,10 @@ import com.app.dto.user.UserDupCheck;
 import com.app.service.user.UserService;
 import com.app.util.LoginManager;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class CustomerController {
 
 	
@@ -76,6 +79,8 @@ public class CustomerController {
 								//				자동으로 객체형태로 파싱되서 데이터가 담겨진다
 		System.out.println("/customer/checkDupIdjson");
 		System.out.println(userDupCheck);
+		
+		log.info("checkDupIdJson 아이디 중복체크 : {}",userDupCheck);
 		
 		//처리 로직
 		//클라이언트에서 보낸 데이터가 중복된 사용자 아이디인지 체크

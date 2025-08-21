158720e04f889daa9e8681a0353cf7b58a8923b5
winjun1128 <hsj1128ty@naver.com>
2025-08-21 15:27:34 +0900

    [React Spring] react 요청 통신용 REST API 통신

---
+++ src/main/java/com/app/dto/study/DrinkItem.java
@@ -0,0 +1,27 @@
+package com.app.dto.study;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrinkItem {
+	String name;
+	String type;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
